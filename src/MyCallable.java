e7d7a1fd1a7b82688fc4f1b6ff6da0491ba5fdf9
MariaChowdhury <maria.chowdhury@gmail.com>
2017-05-31 10:09:19 -0700

    updated value for "-"

--- src/MyCallable.java
+++ src/MyCallable.java
@@ -20,16 +20,14 @@ public class MyCallable implements Callable<DoubleAndId> {
     public DoubleAndId call() throws Exception {
     	line=line.replace("/", "-1");
     	line=line.replace("X", "10");
-    	line=line.replace("-", "");
+    	line=line.replace("-", "0");
        String arr[]=line.split(",");
        double[] stat=new double[arr.length];
        double num=0;String id=arr[0];
        for(int i=1;i<arr.length;i++){
-    	  if(!arr[i].isEmpty()){
     		  arr[i] = arr[i].replaceAll("\\s","");
         	  num=Double.parseDouble(arr[i]);
         	  stat[i]=num;
-       }
     	   	
     	   }
        DoubleAndId did=new DoubleAndId(stat,id);
